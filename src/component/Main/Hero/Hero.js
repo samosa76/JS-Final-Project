176bd1aba22c2ff07fe7572cee81ef59ba58d710
Sam_76 <muhammadusamah76@gmail.com>
2024-10-25 19:48:08 +0700

    Banner With Glassmorphism Card

--- src/component/Main/Hero/Hero.js
+++ src/component/Main/Hero/Hero.js
@@ -1,4 +1,5 @@
 import style from "./Hero.module.css";
+// import down_arrow from "../../../../public/icon/down-arrow.png"
 
 function Hero() {
     //get screen width
@@ -10,10 +11,16 @@ function Hero() {
             <div className={style.image}>
                 <img src={url} className={style.image} />
                 <div className={`${style.title}`}>
-                    <h1 style={{textAlign:"center"}}>Title</h1>
+                    <div style={{textAlign:'center'}}>
+                        <h1>Title</h1>
+                        <p>Ini adalah paragraph singkat dan padat dan hanya di gunakan untuk persobaan</p>
+                    </div>
+
+                    <div className={style.card}>
+                        <img src='/icon/down-arrow.png' className={style.icon}/>
+                    </div>
                 </div>
             </div>
-
         </div>
     );
 }
