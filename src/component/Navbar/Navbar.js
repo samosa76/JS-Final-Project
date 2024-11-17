25f4c12702a5c8846e04962d8971d78defe8502a
Sam_76 <muhammadusamah76@gmail.com>
2024-11-17 23:29:21 +0700

    adding break point for navbar

--- src/component/Navbar/Navbar.js
+++ src/component/Navbar/Navbar.js
@@ -1,19 +1,30 @@
+import { useState } from 'react';
 import style from './Navbar.module.css'
 
 function Navbar() {
+    const [showMenu, setShowMenu] = useState(false);
+
+    const toggleMenu = () => {
+        setShowMenu(!showMenu); // Toggle nilai showMenu ketika gambar diklik
+    };
+
     return (
-        <div className={style.container}>
+        <div className={style.container} id='nav-menu'>
             <nav className={style.navbar}>
+
                 <div>
                     <h1>Navbar</h1>
                 </div>
-                <ul>
+
+                <ul className={`${style.nav_list_menu} ${showMenu ? style.show_menu : ''}`}>
                     <li>Home</li>
                     <li>Courses</li>
                     <li>E-Book</li>
                     <li>About</li>
                 </ul>
-                
+
+
+                <img src='icon/list.png' className={style.icon_nav_menu} onClick={toggleMenu}></img>
 
             </nav>
         </div>
--- src/utils/data/data_books.js
+++ src/utils/data/data_books.js
@@ -14,6 +14,11 @@ const data = [
         desc: 'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Distinctio laborum quisquam harum unde nobis voluptate praesentium quos autem, odio quam ratione esse dolorum repellendus molestiae accusamus modi sequi corporis incidunt.',
         genre: ['Genre 1', 'Genre 2', 'Genre 3'],
     },
+    {
+        title: 'New Books',
+        desc: 'odio quam ratione esse dolorum repellendus molestiae accusamus modi sequi corporis incidunt.',
+        genre: ['Genre 1', 'Genre 2', 'Genre 3'],
+    },
 
 ]
 
